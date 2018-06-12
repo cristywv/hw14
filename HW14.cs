b106d069c125fa63f0b0318eea37cd644c209e3a
cristywv <38766734+cristywv@users.noreply.github.com>
2018-06-12 13:00:02 -0500

    Add files via upload

---
+++ HW14.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hw14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string b = "Cristy";
+            float c = 1.1F;
+            double d = 2.2d;
+            bool e = true;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine("My name is {0} and my favorite number is {1}. This is a {2} statement.", b, a, e);
+
+            Console.WriteLine("What is your favorite color? ");
+            string color = Console.ReadLine();
+            Console.WriteLine(color);
+
+            Console.WriteLine(a < c);
+            Console.WriteLine(c > d);
+            Console.WriteLine("Love" != "Hate");
+            Console.WriteLine("Love" == "Love");
+
+            Console.ReadKey();
+        }
+    }
+}

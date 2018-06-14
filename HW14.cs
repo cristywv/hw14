9734cdc4e8ef512727f222f45078b3cb8345e04e
cristywv <38766734+cristywv@users.noreply.github.com>
2018-06-14 15:01:20 -0500

    Add files via upload
    6/14

--- HW14.cs
+++ HW14.cs
@@ -17,12 +17,12 @@ namespace hw14
             Console.WriteLine(c);
             Console.WriteLine(d);
             Console.WriteLine(e);
+        
             Console.WriteLine("My name is {0} and my favorite number is {1}. This is a {2} statement.", b, a, e);
-
-            Console.WriteLine("What is your favorite color? ");
+         
+            Console.Write("What is your favorite color? ");
             string color = Console.ReadLine();
-            Console.WriteLine(color);
-
+       
             Console.WriteLine(a < c);
             Console.WriteLine(c > d);
             Console.WriteLine("Love" != "Hate");
@@ -30,5 +30,28 @@ namespace hw14
 
             Console.ReadKey();
         }
+
+        public void thisPub()
+        {
+            Console.WriteLine("This is public.");
+        }
+
+        private void thisPri()
+        {
+            Console.WriteLine("This is private.");
+        }
+
+        public string z()
+        {
+            string y = "yakkity yakk yak";
+            return y;
+        }
+
+        public void pet( string cats, string dogs)
+        {
+            Console.WriteLine("i have {0} and {1}.", cats, dogs);
+        }
+
+        
     }
 }
